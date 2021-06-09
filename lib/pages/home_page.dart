d97c48fd3ea28e9ae184f9140b872e1bee484784
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-06-09 13:44:21 +0300

    pages refactoring

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
-import 'package:cat_app/widgets/page_with_tabs.dart';
+import 'package:cat_app/pages/cats_page.dart';
+import 'package:cat_app/pages/favorites_page.dart';
+import 'package:cat_app/pages/profile_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,10 +8,36 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageWithTabs(
-      content: Container(
-        child: Text('home page'),
-      ),
-    );
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+              title: Text('Cat App'),
+              centerTitle: true,
+              bottom: TabBar(
+                tabs: [
+                  Tab(
+                    text: 'Cats',
+                  ),
+                  Tab(
+                    text: 'Favorite',
+                  ),
+                  Tab(
+                    text: 'Profile',
+                  )
+                ],
+              )),
+          body: TabBarView(children: [
+            Container(
+              child: CatsPage(),
+            ),
+            Container(
+              child: FavoritesPage(),
+            ),
+            Container(
+              child: ProfilePage(),
+            ),
+          ]),
+        ));
   }
 }
