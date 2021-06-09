82059e93401c8456317b5a9b68989b8aa372f192
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-06-09 13:52:10 +0300

    add all pages

---
+++ lib/pages/cats_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CatsPage extends StatelessWidget {
+  const CatsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('cats'),
+    );
+  }
+}
