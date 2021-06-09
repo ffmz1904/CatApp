d92bdd7f4bef590047c34b1fa6f43651b2cab353
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-06-09 14:27:52 +0300

    add configs for firebase android

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,21 @@ import 'package:cat_app/bloc/user/user_bloc.dart';
 import 'package:cat_app/bloc/user/user_state.dart';
 import 'package:cat_app/pages/auth_page.dart';
 import 'package:cat_app/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Cat App",
-      // initialRoute: "/",
-      // routes: {
-      //   "/": (context) => HomePage(),
-      //   "/auth": (context) => AuthPage(),
-      // },
       home: BlocProvider<UserBloc>(
         create: (context) => UserBloc(),
         child: BlocBuilder<UserBloc, UserState>(
