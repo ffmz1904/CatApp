de0b9563be2df3c41048526cf30e0ed774bd1a35
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-06-09 13:14:31 +0300

    add home page and auth page

---
+++ lib/pages/auth_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cat App'),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Text('auth page'),
+      ),
+    );
+  }
+}
